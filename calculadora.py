30a0d318042c4e1ee5a996f7f2ec3905b14f5a04
Humberto-Matondo <humbertomat99@gmail.com>
2023-11-13 20:41:12 +0100

    learned about assertions

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,6 @@
 def soma(x,y):
+
+    assert isinstance(x, (int, float)) , 'O valor de X precisa ser inteiro ou float' 
+    assert isinstance(y, (int, float)) , 'O valor de Y precisa ser inteiro ou float'
+    
     return x+y
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from calculadora import soma
 
-# print(soma(10, 20))
-# print(soma(-10, 30))
-# print(soma(1.31, 20.012))
-# print(soma(1.5, 2.6))
+#ISSO E PARA QUANDO APARECER O ERRO, O DEV CONSEGUIR PERCEBER DE CONCRETO O QUE SE ESTA A CONTECER:
+try:
+    print(soma('1.5', 2.6))
+except AssertionError as e:
+    print(f'Conta Invalida:{e}')
+
+print(soma(1.31, 20.012))
