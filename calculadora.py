591f5febf1cf3db01617669438cacddc26d282fb
Humberto-Matondo <humbertomat99@gmail.com>
2023-11-13 21:27:32 +0100

    learned about Doctest vol2

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,35 @@
 def soma(x,y):
     """
         soma x e y
-        
-        >>> soma(10, 20)
-        30 
+                
         >>> soma(-15, 20)
         5
-        >>> soma(5, 20)
         
+        >>> soma(10, 20)
+        30
     """
 
     assert isinstance(x, (int, float)) , 'O valor de X precisa ser inteiro ou float' 
     assert isinstance(y, (int, float)) , 'O valor de Y precisa ser inteiro ou float'
     
     return x+y
+
+def subtrai(x,y):
+    """
+        subtrai x e y
+        
+        >>> subtrai(10, 5)
+        5
+        
+        >>> subtrai('10', 5)
+        Traceback (most recent call last):
+        AssertionError: O valor de X precisa ser inteiro ou float
+    """
+    assert isinstance(x, (int, float)) , 'O valor de X precisa ser inteiro ou float' 
+    assert isinstance(y, (int, float)) , 'O valor de Y precisa ser inteiro ou float'
+    
+    return x-y
+
 
 if __name__ == '__main__':
     import doctest
